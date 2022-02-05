d25ca54eec12ad443f59d82febb03f98411aae83
SangYeonY <sa9798@naver.com>
2022-02-05 14:29:43 +0900

    java

---
+++ day14/src/koreait/day14/Puppy.java
@@ -0,0 +1,25 @@
+package koreait.day14;
+
+public class Puppy extends Animal{
+	
+	static final String TYPE="강아지";
+	
+	public Puppy() {
+	}
+	
+	public Puppy(String name,String color) {
+		super(name,color);
+	}
+	
+	@Override
+	public void sound() {  //인스턴스 메소드는 현재 객체의 인스턴스 필드값으로 처리를 합니다.
+		// 추상메소드 구현합니다.
+		System.out.println("강아지🐶🐶🐶는 멍멍하고 짖습니다.");
+	}
+	
+	@Override
+	public String toString() {		
+		return super.toString() +  TYPE;   //super는 Animal 클래스 toString
+	}
+
+}
